9e53ad5dee1abc29d6d65210f5aa360dacb958c1
DESKTOP-1RH0QUC\magip <magipripmail.goo.ne.jp>
2020-05-13 00:01:33 +0900

    rankcorcoefライブラリクレートに平均を出力する関数averageを追加

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,6 +1,13 @@
 extern crate ranktest;
 extern crate rankcorcoef;
 
+use rankcorcoef::average;
+
 fn main() {
-    println!("Hello, world!");
+    let data : Vec<i32> = vec![1,2,3,4,5,6];
+    let ave : i32;
+
+    ave = average(data);
+
+    println!("{}",ave);
 }
--- rankcorcoef/src/lib.rs
+++ rankcorcoef/src/lib.rs
@@ -5,3 +5,25 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
 }
+
+#[no_mangle]
+pub extern "stdcall" fn Spearman(count: i32) -> i32 {
+    if count < 0 {
+       println!("A negative count is not supported.");
+        return 0;
+    }
+
+    return 1;
+}
+
+#[no_mangle]
+pub fn average(mut ope: Vec<i32>) -> i32 {
+    let mut average : i32 = 0;
+
+    for i in &mut ope {
+        average += *i;
+    }
+    average /= ope.len() as i32;
+
+    return average;
+}
