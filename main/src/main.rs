e500e6c6f00abf74dc64667938b92720e6452f7d
DESKTOP-1RH0QUC\magip <magipripmail.goo.ne.jp>
2020-05-16 17:23:31 +0900

    rankcorcoefライブラリクレートにケンドールの順位相関係数を出力する関数kendall_coefficientを追加

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,13 +1,14 @@
 extern crate ranktest;
 extern crate rankcorcoef;
 
-use rankcorcoef::average;
+use rankcorcoef::kendall_coefficient;           // ケンドールの順位相関係数
 
 fn main() {
-    let data : Vec<i32> = vec![1,2,3,4,5,6];
-    let ave : i32;
+    let datax : Vec<f32> = vec![23.6,25.3,18.7,21.3,16.9,20.8];
+    let datay : Vec<f32> = vec![118.0,111.0,116.0,128.0,109.0,121.0];
+    let ave : f32;
 
-    ave = average(data);
+    ave = kendall_coefficient(datax,datay);
 
     println!("{}",ave);
 }
--- rankcorcoef/src/lib.rs
+++ rankcorcoef/src/lib.rs
@@ -17,13 +17,65 @@ pub extern "stdcall" fn Spearman(count: i32) -> i32 {
 }
 
 #[no_mangle]
-pub fn average(mut ope: Vec<i32>) -> i32 {
-    let mut average : i32 = 0;
+// ケンドールの順位相関係数
+// x : 順位データ1
+// y : 順位データ2
+pub fn kendall_coefficient(mut x: Vec<f32>, mut y: Vec<f32>) -> f32 {
+    // ケンドールの順位相関係数
+    let mut coefficient : f32;
+
+    // ２つの順位データの個数が異なっているならエラー終了
+    assert_eq!( x.len(), y.len() );
+    if x.len() != y.len() {
+        return 0.0
+    }
+
+    // xとyのベクターの要素を昇順に直す
+    // バブルソートする
+    let mut work : f32;
+    for i in 0..x.len() {
+        for j in 0..x.len() {
+            if x[i] < x[j] {
+                work = x[i];
+                x[i] = x[j];
+                x[j] = work;
+
+                work = y[i];
+                y[i] = y[j];
+                y[j] = work;
+            }
+        }
+    }
+
+    // y[i] < y[j] (i<j) となるyの個数を数える
+    let mut supcount : i32 = 0 ;
+    for i in 0..x.len() {
+        for j in (i+1)..x.len() {
+            if y[i] < y[j] {
+                supcount += 1;
+            }
+        }
+    }
+
+    // 相関係数の演算
+    coefficient = 4.0;
+    coefficient *= supcount as f32;
+    coefficient /=  x.len() as f32 ;
+    coefficient /= ( x.len() as f32 - 1.0 ) as f32;
+    coefficient -= 1.0;
+
+
+    return coefficient;
+}
+
+#[no_mangle]
+pub fn average(mut ope: Vec<f32>) -> f32 {
+    let mut average : f32 = 0.0;
 
     for i in &mut ope {
         average += *i;
     }
-    average /= ope.len() as i32;
+    average /= ope.len() as f32;
 
     return average;
 }
